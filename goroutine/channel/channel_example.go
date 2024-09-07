3feee1c833c026cbccbc059d1ffd24cda1e02b6c
rla1wo23@gmail.com <rla1wo23@gmail.com>
2024-09-07 12:33:32 +0900

    comments: add comments for explanation

--- goroutine/channel/channel_example.go
+++ goroutine/channel/channel_example.go
@@ -7,12 +7,12 @@ import (
 func pinger(c chan string) {
     for i := 0; i < 5; i++ {
         fmt.Println("Sending ping...")
-        c <- "ping" // 값을 보내고, 받는 쪽이 받을 때까지 대기
+        c <- "ping" // 값을 보내고, 받는 쪽이 받을 때까지 대기. 즉, 채널 변수는 버퍼를 상대방이 비울 때 까지 기다린다.
     }
 }
 func printer(c chan string) {
     for i := 0; i < 5; i++ {
-        msg := <-c // 값을 받을 때까지 대기
+        msg := <-c // 값을 받을 때까지 대기. c 버퍼에 뭐가 들어가기 전까지 기다림. lock과 signal 구조가 추상화 돼 있다.
         fmt.Println("Received:", msg)
         time.Sleep(time.Second * 1) // 1초 대기
     }
